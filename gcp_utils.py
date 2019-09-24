f35aa07ae18f4253c0fd5d33877bb4eff90761e5
josh <jmjoseph@mit.edu>
2019-09-24 16:01:15 -0400

    minor code refactoring

--- gcp_utils.py
+++ gcp_utils.py
@@ -12,7 +12,7 @@ def list_files(gcp_bucket_name, prefix):
     return [f.name for f in blobs]
 
 
-def copy_dataset_locally_if_missing(dataset_remote_source, local_dataset_dir):
-    if not os.path.exists(local_dataset_dir.as_posix()):
-        local_dataset_dir.mkdir(parents=True, exist_ok=True)
-        os.system("gsutil -m cp -r '{}' '{}'".format(dataset_remote_source, local_dataset_dir.as_posix()))
+def copy_folder_locally_if_missing(folder_remote_source, local_folder_dir):
+    if not os.path.exists(local_folder_dir.as_posix()):
+        local_folder_dir.mkdir(parents=True, exist_ok=True)
+        os.system("gsutil -m cp -r '{}' '{}'".format(folder_remote_source, local_folder_dir.as_posix()))
