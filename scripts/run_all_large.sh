43f5d8d38ec93dc40c97f9aec2d25d41f90d91dd
Josh Joseph <josh.m.joseph@gmail.com>
2019-10-30 18:52:51 -0400

    added bash comment to the two scripts

--- scripts/run_all_large.sh
+++ scripts/run_all_large.sh
@@ -1,3 +1,6 @@
+#!/bin/bash
+
+
 pipenv run python3 prepare_dataset.py --config-file configs/dataset_config-large.yaml
 pipenv run python3 train_segmentation_model.py --config-file configs/train_config-large.yaml
 pipenv run python3 test_segmentation_model.py --gcp-bucket gs://necstlab-sandbox --dataset-id dataset-large --model-id segmentation-model-large_20190924T180419Z
--- scripts/run_all_small.sh
+++ scripts/run_all_small.sh
@@ -1,3 +1,6 @@
+#!/bin/bash
+
+
 pipenv run python3 prepare_dataset.py --config-file configs/dataset_config-small.yaml
 pipenv run python3 train_segmentation_model.py --config-file configs/train_config-small.yaml
 pipenv run python3 test_segmentation_model.py --gcp-bucket gs://necstlab-sandbox --dataset-id dataset-small --model-id segmentation-model-small_20190924T191717Z
