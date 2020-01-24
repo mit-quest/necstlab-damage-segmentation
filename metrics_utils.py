b505783e4e1f9f8a94030ba6ff088740c9dc7f72
rak5216 <55461758+rak5216@users.noreply.github.com>
2020-01-23 23:05:25 -0500

    Update metrics_utils.py

--- metrics_utils.py
+++ metrics_utils.py
@@ -2,7 +2,7 @@ from keras.metrics import Metric as MetricKeras, Accuracy
 from keras.metrics import FalsePositives, TruePositives, TrueNegatives, FalseNegatives, Precision, Recall
 from keras import backend as K
 from keras.utils import metrics_utils as metrics_utils_keras
-from segmentation_models.base import Metric as MetricSM, KerasObject, functional
+from segmentation_models.base import Metric as MetricSM, functional
 import numpy as np
 
 
@@ -277,6 +277,8 @@ class OneHotFBetaScore(FBetaScore):
         return super().__call__(groundtruth, prediction_onehot, **kwargs)
 
 
+# based on Keras precision and recall class definitions found at:
+# https://github.com/keras-team/keras/blob/7a39b6c62d43c25472b2c2476bd2a8983ae4f682/keras/metrics.py#L1154
 class IoUScore(MetricKeras):
     """Computes the mean Intersection-Over-Union metric.
     Intersection-Over-Union is a common evaluation metric for semantic image
@@ -409,8 +411,131 @@ class OneHotIoUScore(IoUScore):
         return super().__call__(groundtruth, prediction_onehot, **kwargs)
 
 
+# VERSION 2 CLASSBINARYACCURACY METHOD, BASED ON KERAS PACKAGE -- ACCUMULATED OVER EPOCH (inherit KERAS.METRIC)
+# based on Keras precision and recall class definitions found at:
+# https://github.com/keras-team/keras/blob/7a39b6c62d43c25472b2c2476bd2a8983ae4f682/keras/metrics.py#L1154
+class ClassBinaryAccuracyKeras(MetricKeras):
+    r"""
+    .. math:: Binary Accuracy = (TN + TP)/(TN+TP+FN+FP) = Number of correct assessments/Number of all assessments,
+    for given class for more than one class input, output becomes mean accuracy (similar but not same as categorical)
+    # Arguments
+        thresholds: (Optional) A float value or a python list/tuple of float
+            threshold values in [0, 1]. A threshold is compared with prediction
+            values to determine the truth value of predictions (i.e., above the
+            threshold is `true`, below is `false`). One metric value is generated
+            for each threshold value. If neither thresholds nor top_k are set, the
+            default is to calculate precision with `thresholds=0.5`.
+        top_k: (Optional) Unset by default. An int value specifying the top-k
+            predictions to consider when calculating precision.
+        class_id: (Optional) Integer class ID for which we want binary metrics.
+            This must be in the half-open interval `[0, num_classes)`, where
+            `num_classes` is the last dimension of predictions.
+        name: (Optional) string name of the metric instance.
+        dtype: (Optional) data type of the metric result.
+    """
+    def __init__(self,
+                 thresholds=None,
+                 top_k=None,
+                 class_id=None,
+                 name=None,
+                 dtype=None):
+        self.name = name or 'class_all_binary_accuracy_keras'
+        super().__init__(name=name, dtype=dtype)
+        self.init_thresholds = thresholds
+        if top_k is not None and K.backend() != 'tensorflow':
+            raise RuntimeError(
+                '`top_k` argument for `Precision` metric is currently supported '
+                'only with TensorFlow backend.')
+
+        self.top_k = top_k
+        self.class_id = class_id
+
+        default_threshold = 0.5 if top_k is None else metrics_utils_keras.NEG_INF
+        self.thresholds = metrics_utils_keras.parse_init_thresholds(
+            thresholds, default_threshold=default_threshold)
+        self.true_positives = self.add_weight(
+            'true_positives',
+            shape=(len(self.thresholds),),
+            initializer='zeros')
+        self.false_positives = self.add_weight(
+            'false_positives',
+            shape=(len(self.thresholds),),
+            initializer='zeros')
+        self.false_negatives = self.add_weight(
+            'false_negatives',
+            shape=(len(self.thresholds),),
+            initializer='zeros')
+        self.true_negatives = self.add_weight(
+            'true_negatives',
+            shape=(len(self.thresholds),),
+            initializer='zeros')
+
+    def update_state(self, y_true, y_pred, sample_weight=None):
+        return metrics_utils_keras.update_confusion_matrix_variables(
+            {
+                metrics_utils_keras.ConfusionMatrix.TRUE_POSITIVES: self.true_positives,
+                metrics_utils_keras.ConfusionMatrix.FALSE_POSITIVES: self.false_positives,
+                metrics_utils_keras.ConfusionMatrix.FALSE_NEGATIVES: self.false_negatives,
+                metrics_utils_keras.ConfusionMatrix.TRUE_NEGATIVES: self.true_negatives
+            },
+            y_true,
+            y_pred,
+            thresholds=self.thresholds,
+            top_k=self.top_k,
+            class_id=self.class_id,
+            sample_weight=sample_weight)
+
+    def result(self):
+        denom = (self.true_positives + self.false_negatives + self.false_positives + self.true_negatives)
+        result = K.switch(
+            K.greater(denom, 0),
+            (self.true_positives + self.true_negatives) / denom,
+            K.zeros_like(self.true_positives + self.true_negatives))
+
+        return result[0] if len(self.thresholds) == 1 else result
+
+    def reset_states(self):
+        num_thresholds = len(metrics_utils_keras.to_list(self.thresholds))
+        K.batch_set_value(
+            [(v, np.zeros((num_thresholds,))) for v in self.weights])
+
+    def get_config(self):
+        config = {
+            'thresholds': self.init_thresholds,
+            'top_k': self.top_k,
+            'class_id': self.class_id
+        }
+        base_config = super().get_config()
+        return dict(list(base_config.items()) + list(config.items()))
+
+
+class OneHotClassBinaryAccuracyKeras(ClassBinaryAccuracyKeras):
+    def __init__(self,
+                 thresholds=None,
+                 top_k=None,
+                 class_id=None,
+                 name=None,
+                 dtype=None):
+        self.name = name or 'class_all_binary_accuracy_keras_1H'
+        super().__init__(
+            thresholds=thresholds,
+            top_k=top_k,
+            class_id=class_id,
+            name=self.name,
+            dtype=dtype)
+
+    # call redirects to parent class following one hot conversion
+    def __call__(self, groundtruth, prediction, **kwargs):  # assuming 4D tensor is BHWC
+        # based on keras.metrics.categorical_accuracy to determine max pred index (1 of channels) at each HW location
+        prediction_onehot_indices = K.argmax(prediction, axis=-1)
+        prediction_onehot = K.one_hot(prediction_onehot_indices,
+                                      K.int_shape(prediction)[-1])  # assume 4D tensor is BHWC
+        return super().__call__(groundtruth, prediction_onehot, **kwargs)
+
+
+# VERSION 1 CLASSBINARYACCURACY METHOD, BASED ON SEGMENTATION_MODELS PACKAGE -- AVERAGED OVER EPOCH
 # adapted from: s_m.IOUScore() from github.com/qubvel/segmentation_models/blob/master/segmentation_models/metrics.py
-class ClassBinaryAccuracy(MetricSM):
+class ClassBinaryAccuracySM(MetricSM):
     r"""
     .. math:: Binary Accuracy = (TN + TP)/(TN+TP+FN+FP) = Number of correct assessments/Number of all assessments,
     for given class for more than one class input, output becomes mean accuracy (similar but not same as categorical)
@@ -437,7 +562,7 @@ class ClassBinaryAccuracy(MetricSM):
             smooth=SMOOTH,
             name=None
     ):
-        self.name = name or 'class_all_binary_accuracy'
+        self.name = name or 'class_all_binary_accuracy_sm'
         super().__init__(name=self.name)
         self.class_weights = class_weights if class_weights is not None else 1
         self.class_indexes = class_indexes
@@ -464,7 +589,7 @@ class ClassBinaryAccuracy(MetricSM):
         return score
 
 
-class OneHotClassBinaryAccuracy(ClassBinaryAccuracy):
+class OneHotClassBinaryAccuracySM(ClassBinaryAccuracySM):
     def __init__(
             self,
             class_weights=None,
@@ -474,7 +599,7 @@ class OneHotClassBinaryAccuracy(ClassBinaryAccuracy):
             smooth=SMOOTH,
             name=None
     ):
-        self.name = name or 'class_all_binary_accuracy_1H'
+        self.name = name or 'class_all_binary_accuracy_sm_1H'
         super().__init__(
             class_weights=class_weights,
             class_indexes=class_indexes,
