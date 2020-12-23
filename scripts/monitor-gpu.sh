fe91084fd911884e2f10b72efdd024a594004007
Carolina Furtado <cfurtado@mit.edu>
2020-12-23 13:21:32 -0500

    fixed gpu-monitor that stopped working with the python upgrade
    the python upgrade lead to the use of the lastes version of google-cloud-monitoring, that does not work with the repository that we were using. So I downloaded the repository and changed the requirements by hand

--- scripts/monitor-gpu.sh
+++ scripts/monitor-gpu.sh
@@ -1,14 +1,6 @@
 ## allow monitoring gpu performance
-if [ -d "/monitor-gpu" ]
-then
-    cd monitor-gpu/gcp-gpu-utilization-metrics
-    python3 ./report_gpu_metrics.py &
-    echo "GPU monitoring ON"
-else
-    mkdir -p monitor-gpu
-    cd monitor-gpu
-    git clone https://github.com/b0noI/gcp-gpu-utilization-metrics.git
-    cd gcp-gpu-utilization-metrics && pip3 install -r requirenments.txt
-    python3 ./report_gpu_metrics.py &
-    echo "GPU monitoring ON"
-fi
+## scripts in monitor_gpu folder taken from https://github.com/b0noI/gcp-gpu-utilization-metrics
+## fixed requirements (not fixed in the git, and that leads to errors)
+cd scripts/monitor_gpu && pip3 install -r requirenments.txt
+python3 ./report_gpu_metrics.py &
+echo "GPU monitoring ON"
